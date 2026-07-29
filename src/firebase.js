1b10d1367055d5d5d98cfd7e7db8e28c3ce568c6
ilyas-ilz <xbeatlover@gmail.com>
2026-07-29 10:55:00 +0530

    refactor: update Firebase configuration to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth, GoogleAuthProvider, OAuthProvider, signInWithPopup, signOut, o
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAWyTreHqwF5X_AAAnP9CeIRQ-ggMcOtBY",
-  authDomain: "thafheem-ul-quran.firebaseapp.com",
-  projectId: "thafheem-ul-quran",
-  storageBucket: "thafheem-ul-quran.firebasestorage.app",
-  messagingSenderId: "69349166270",
-  appId: "1:69349166270:web:2ffde7a6faea461edb62ef",
-  measurementId: "G-D7NNTSQSD2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
